fba2eb6fe559a91b6d6f577c6f05a9c96dd8aee0
furkantokac <developmentft@gmail.com>
2017-07-31 05:27:58 +0300

    Base functions are written.

--- src/database.py
+++ src/database.py
@@ -1,2 +1,94 @@
 # -*- coding: utf-8 -*-
 
+from pymongo import MongoClient
+from datetime import datetime
+
+
+class MongoDatabase:
+    # -------------- BEG PRIVATE
+    def __init__(self):
+        # self._connect()
+        # self.db = self._get_database("Freg_test")
+        # Collections class It just stores collection names
+        class Collections:
+            def __init__(self):
+                #  if you have new collection, put it here as below
+                self.member = "Member"
+                self.department = "Department"
+
+        self.colls = Collections()
+        self.dbName = ""
+
+    def _connect(self, databaseName):
+        self.dbName = databaseName
+        try:
+            self.client = MongoClient()
+            self.db = self._get_database(self.dbName)
+            return True
+        except:
+            return False
+
+    def _get_database(self, dbname):
+        return self.client[dbname]
+
+    def _get_collection(self, collname):
+        return self.db[collname]
+
+    # -------------- BEG GENERALS
+    def insert_element(self, collection, field):
+        coll = self._get_collection(collection)
+        coll.insert(field)
+
+    # RET : number of updated field
+    def update_field(self, collection, find_query, update_query):
+        coll = self._get_collection(collection)
+        return coll.update(find_query, {"$set": update_query})["n"]
+
+    # RET : number of updated field
+    def push_item_to_array(self, collection, find_query, item):
+        coll = self._get_collection(collection)
+        return coll.update(find_query, {"$push": item})["n"]
+
+    def pull_item_from_array(self, collection, find_query, item):
+        coll = self._get_collection(collection)
+        return coll.update(find_query, {"$pull": item})["n"]
+
+    # RET : first result of the query
+    def query_result_single(self, collection, query):
+        coll = self._get_collection(collection)
+        return coll.find_one(query)
+
+    # RET : list of the result of the query
+    def query_result_multi(self, collection, query):
+        coll = self._get_collection(collection)
+        return coll.find(query)
+
+    # RET : number of query result
+    def count_query_result(self, collection, query):
+        coll = self._get_collection(collection)
+        return coll.find(query).explain()["n"]
+
+    def get_datetime(self):
+        datetime_format = "%H:%M %d.%m.%y"
+        return datetime.now().strftime(datetime_format)
+
+    def str_to_datetime(self, str_dt):
+        datetime_format = "%H:%M %d.%m.%y"
+        return datetime.strptime(str_dt, datetime_format)
+
+    # -------------- BEG MEMBER
+    def add_new_member(self, firstname="", lastname="", email="", department="", mobilencc="", mobiletr=""):
+        member = {
+            "first_name": firstname,
+            "last_name": lastname,
+            "email": email,
+            "department": department,
+            "mobile_ncc": mobilencc,
+            "mobile_tr": mobiletr,
+            "registration_date": self.get_datetime()
+        }
+
+        self.insert_element(self.colls.member, member)
+
+    def delete_member_by_email(self, email):
+        pass  # TODO
---
+++ src/freg.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+
+import mainwindow, sys
+from database import MongoDatabase
+from PyQt5.QtWidgets import QMainWindow, QApplication
+
+__version__ = "0.0"
+
+
+class Frec(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.ui = mainwindow.Ui_Frec()
+        self.ui.setupUi(self)
+
+        self.db = MongoDatabase()
+
+        self.init_frec()
+
+    def init_frec(self):
+        self.ui.btn_register.clicked.connect(self.save_new_member)
+        self.ui.btn_remove.clicked.connect(self.delete_member)
+
+        self.ui.btn_clear.clicked.connect(self.clear_form)
+        self.ui.btn_export.clicked.connect(self.export_cvs)
+        self.ui.btn_import.clicked.connect(self.import_cvs)
+
+        self.ui.btn_connectDb.clicked.connect(self.connect_db)
+
+        self.connect_db()
+
+    def show_message(self, msg, timeout=3000):
+        self.ui.statusBar.showMessage(msg, timeout)
+
+    def connect_db(self):
+        dbName = self.ui.lne_dbName.text()
+        if self.db._connect( dbName ):
+            self.show_message("Database connection with '"+dbName+"' is successful.")
+            self.ui.lbl_dbConnection.setText("Database : Connected '"+dbName+"'")
+            self.ui.lbl_dbConnection.setStyleSheet('color: green')
+        else:
+            self.ui.lbl_dbConnection.setText("Database : Disconnected")
+            self.ui.lbl_dbConnection.setStyleSheet('color: red')
+            self.show_message("Database connection couldn't established!")
+            return False
+
+        return True
+
+    # Save new member to database
+    def save_new_member(self):
+        pass  # TODO
+
+    # Delete chosen member on tableView
+    def delete_member(self):
+        pass  # TODO
+
+    # Clear inside the line edits in registration form
+    def clear_form(self):
+        self.ui.lne_email.setText("")
+        self.ui.lne_firstname.setText("")
+        self.ui.lne_lastname.setText("")
+        self.ui.lne_mobilecyp.setText("")
+        self.ui.lne_mobiletr.setText("")
+        self.ui.comboBox_department.setCurrentIndex(0)
+
+    # Export members in the database as csv
+    def export_cvs(self):
+        pass  # TODO
+
+    # Import members from a csv file
+    def import_cvs(self):
+        pass  # TODO
+
+    def create_desktop_entry(self):
+        # TODO: Replace [-HOMEDIR-] inside ./data/freg.desktop and copy it to ~/.local/share/applications/
+        pass
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    myapp = Frec()
+
+    myapp.show()
+    sys.exit(app.exec_())
